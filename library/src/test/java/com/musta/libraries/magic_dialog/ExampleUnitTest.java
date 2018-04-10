975ae89aa8ea49a96ad4170a2e4ef652522cf11b
mustafizpi <mustafizur.rahman@pistrategy.org>
2018-04-10 15:34:34 +0600

    module name changed to library

--- library/src/test/java/com/musta/libraries/magic_dialog/ExampleUnitTest.java
+++ library/src/test/java/com/musta/libraries/magic_dialog/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.musta.libraries.magic_dialog;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
