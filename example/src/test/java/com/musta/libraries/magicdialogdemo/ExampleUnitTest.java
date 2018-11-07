c0678372f61f07f25c98c9ed34ec78f6285bb555
Md. Mustafizur Rahman <mustafiz012@outlook.com>
2018-11-07 22:40:26 +0600

    'app' name changed to 'example'

--- example/src/test/java/com/musta/libraries/magicdialogdemo/ExampleUnitTest.java
+++ example/src/test/java/com/musta/libraries/magicdialogdemo/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.musta.libraries.magicdialogdemo;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
